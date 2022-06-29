0b2b7dc6fe4330f3fd30988f5b0ce2cd59ca1fe2
Andrey Timush <andrikshumit@gmail.com>
2022-06-29 18:43:49 +0300

    init project

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css" type="text/css">
+</head>
+<body>
+    
+    <!-- Форма авторизации-->
+
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите пароль">
+        <button>Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="#"> Зарегистрируйтесь </a>
+    </form>
+    
+</body>
+</html>
